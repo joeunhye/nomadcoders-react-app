80a15225b02b90dffd0d87669718c803b85faad3
joeunhye <masimel33@gmail.com>
2022-03-09 23:12:48 +0900

    useEffect study

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import {useState, useEffect} from 'react'
 
 function App() {
 	const [counter, setCounter] = useState(0);
+	const [keyword, setKeyword] = useState('')
 	const onClick = () => setCounter(prev => prev + 1)
-	console.log('i run all the time')
-	const iRunOnlyOne = () => {
-		console.log('i run only once')
-	}
-	useEffect(iRunOnlyOne, [])
+	const onChange = (e) => setKeyword(e.target.value);
+	console.log('I run all the time')
+	useEffect(() => {
+		console.log('I run only once.')
+	}, [])
+	useEffect(() => {
+		if(keyword !== '' && keyword.length > 5) {
+			console.log('I run when "keyword" changes.', keyword)
+		}
+	}, [keyword])
+	useEffect(() => {
+		console.log('I run when "counter" changes.')
+	}, [counter])
+	useEffect(() => {
+		console.log('I run when "keyword & counter" changes.')
+	}, [keyword, counter])
 	return ( 
 		<div>
+			<input type="text" placeholder="Search here..." value={keyword} onChange={onChange} />
 			<h1 className={styles.title}>{counter}</h1>
 			<Button text={'Continue'} />
 			<button onClick={onClick}>click me</button>
