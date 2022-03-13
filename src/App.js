b9ebc789569964c68d2ebbb311b319f8d2c219c8
joeunhye <masimel33@gmail.com>
2022-03-13 23:14:49 +0900

    useEffect study

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import Button from "./Button";
-import styles from './App.module.css';
-import {useState, useEffect} from 'react'
+import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
+function Hello() {
+	useEffect(() => {
+		console.log("Created 😉");
+		return () => console.log("Destroyed 😢");
+	}, []);
+	return <h1>Hello~😊</h1>;
+}
 
 function App() {
 	const [counter, setCounter] = useState(0);
-	const [keyword, setKeyword] = useState('')
-	const onClick = () => setCounter(prev => prev + 1)
-	const onChange = (e) => setKeyword(e.target.value);
-	console.log('I run all the time')
+	const [keyword, setKeyword] = useState("");
+	const [showing, setShowing] = useState(false);
+	const onClick = () => setCounter(prev => prev + 1);
+	const onChange = e => setKeyword(e.target.value);
+	const onBtnChange = () => setShowing(value => !value);
+	console.log("I run all the time");
 	useEffect(() => {
-		console.log('I run only once.')
-	}, [])
+		console.log("I run only once.");
+	}, []);
 	useEffect(() => {
-		if(keyword !== '' && keyword.length > 5) {
-			console.log('I run when "keyword" changes.', keyword)
+		if (keyword !== "" && keyword.length > 5) {
+			console.log('I run when "keyword" changes.', keyword);
 		}
-	}, [keyword])
+	}, [keyword]);
 	useEffect(() => {
-		console.log('I run when "counter" changes.')
-	}, [counter])
+		console.log('I run when "counter" changes.');
+	}, [counter]);
 	useEffect(() => {
-		console.log('I run when "keyword & counter" changes.')
-	}, [keyword, counter])
-	return ( 
+		console.log('I run when "keyword & counter" changes.');
+	}, [keyword, counter]);
+	return (
 		<div>
 			<input type="text" placeholder="Search here..." value={keyword} onChange={onChange} />
 			<h1 className={styles.title}>{counter}</h1>
-			<Button text={'Continue'} />
+			<Button text={"Continue"} />
 			<button onClick={onClick}>click me</button>
+			{showing ? <Hello /> : null}
+			<button onClick={onBtnChange}>{showing ? "Hide" : "Show"}</button>
 		</div>
 	);
 }
