06a368d96794aeb93623ee449241fe07f0f904ea
joeunhye <masimel33@gmail.com>
2022-03-14 15:59:22 +0900

    todo list

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ function App() {
 			return;
 		}
 		setToDo("");
-		setTodos(currentArr => [todo, ...currentArr]);
+		setTodos(currentArr => [...currentArr, todo]);
+	};
+	const deleteBtn = e => {
+		const idx = parseInt(e.target.className);
+		setTodos(todos.filter((item, todoIndex) => idx !== todoIndex)); //arr.filter((처리할 현재 요소, 처리할 현재 요소의 인덱스)
 	};
 	console.log(todos);
 	return (
@@ -22,6 +26,17 @@ function App() {
 				<input type="text" value={todo} onChange={onChange} placeholder="Write your to do..." />
 				<button>Add To Do</button>
 			</form>
+			<hr />
+			<ul>
+				{todos.map((item, index) => (
+					<li key={index}>
+						{item}
+						<button className={index} onClick={deleteBtn}>
+							✔
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 }
